966eee9eb39fdfd25e15d43c44398144b8bccdb5
Brandon Shankar <bshankar12@comcast.net>
2020-02-29 12:58:12 -0800

    Updates

---
+++ precache-manifest.82c2ca723097538140d88cf189e38a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79bf20aaf1f22f49e075a5615fd7a22c",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "ff6fdc8a52cd33425327",
+    "url": "/Clicky-Game/static/css/main.e0aaa833.chunk.css"
+  },
+  {
+    "revision": "2b28977e3b6df7765ab2",
+    "url": "/Clicky-Game/static/js/2.f0a6a870.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky-Game/static/js/2.f0a6a870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6fdc8a52cd33425327",
+    "url": "/Clicky-Game/static/js/main.93e5b9ee.chunk.js"
+  },
+  {
+    "revision": "0c44dba98bd5877c6a80",
+    "url": "/Clicky-Game/static/js/runtime-main.297251f5.js"
+  }
+]);
